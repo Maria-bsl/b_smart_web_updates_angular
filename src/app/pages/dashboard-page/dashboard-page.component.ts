20a55ad88e90c1a4154b0458bbc703dab4e0ac46
Maria-bsl <maria@bizlogicsolutions.com>
2024-12-13 17:28:41 +0300

    dashboard updated

--- src/app/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,10 +1,16 @@
-import { Component, Input, signal, ViewEncapsulation } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  signal,
+  ViewEncapsulation,
+} from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { MatIconModule } from '@angular/material/icon';
 import { AppConfigService } from 'src/app/core/services/app-config/app-config.service';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
-import { firstValueFrom, Observable, of } from 'rxjs';
+import { firstValueFrom, map, Observable, of } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { MatRippleModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
@@ -12,6 +18,8 @@ import { HighchartsChartModule } from 'highcharts-angular';
 // import Highcharts from 'highcharts';
 import * as Highcharts from 'highcharts';
 import { ElementDomManipulationService } from 'src/app/core/services/dom-manipulation/element-dom-manipulation.service';
+import { DashboardForm } from 'src/app/core/enums/admin/dashboard.enum';
+import { UnsubscribeService } from 'src/app/core/services/unsubscribe-service/unsubscribe.service';
 
 type CustomChartType = {
   updateFromInput?: boolean;
@@ -38,13 +46,17 @@ type CustomChartType = {
   styleUrl: './dashboard-page.component.scss',
   encapsulation: ViewEncapsulation.Emulated,
 })
-export class DashboardPageComponent {
-  @Input('total-schools') totalSchools: string = '0';
-  @Input('active-schools') activeSchool: string = '0';
-  @Input('bills-issued') billsIssued: string = '0';
-  @Input('students') students: string = '0';
-  @Input('users') users: string = '0';
-  @Input('blocked-users') blockedUsers: string = 'Bokutani';
+export class DashboardPageComponent implements AfterViewInit {
+  totalSchools = signal<string>('');
+  activeSchool = signal<string>('');
+  billsIssued = signal<string>('');
+  students = signal<string>('');
+  users = signal<string>('');
+  bankUsers = signal<string>('');
+  schoolUsers = signal<string>('');
+  collectedAmount = signal<string>('');
+  //('blocked') blocked= signal<string>('');
+  @Input('keys') keys: string = '';
   activeSchoolsChart!: CustomChartType;
   customChartType!: CustomChartType;
   blockedUsers$!: Observable<string[]>;
@@ -53,16 +65,13 @@ export class DashboardPageComponent {
   constructor(
     private tr: TranslateService,
     private appConfig: AppConfigService,
-    private domService: ElementDomManipulationService
+    private domService: ElementDomManipulationService,
+    private unsubscribe: UnsubscribeService
   ) {
     this.tr.use('en');
     this.registerIcons();
     this.initChart();
     this.initActiveSchoolsChart();
-    let arr = this.domService.getCommaSeperatedValuesFromString(
-      this.blockedUsers
-    );
-    this.blockedUsers$ = of(arr);
   }
   private initActiveSchoolsChart() {
     this.activeSchoolsChart = {
@@ -122,5 +131,102 @@ export class DashboardPageComponent {
   private registerIcons() {
     let icons = ['hut', 'airplay'];
     this.appConfig.addIcons(icons, '/assets/icons');
+  }
+  private parseTotalSchools() {
+    const totalSchools$ = this.domService.ids$.pipe(
+      this.unsubscribe.takeUntilDestroy,
+      map((el) => el.get(DashboardForm.TOTAL_SCHOOLS) as HTMLInputElement)
+    );
+    totalSchools$.subscribe({
+      next: (htmlInput) => this.totalSchools.set(htmlInput.value),
+      error: (err) => console.error(err.message),
+    });
+  }
+  private parseActiveSchools() {
+    const activeSchools$ = this.domService.ids$.pipe(
+      this.unsubscribe.takeUntilDestroy,
+      map((el) => el.get(DashboardForm.ACTIVE_SCHOOLS) as HTMLInputElement)
+    );
+    activeSchools$.subscribe({
+      next: (htmlInput) => this.activeSchool.set(htmlInput.value),
+      error: (err) => console.error(err.message),
+    });
+  }
+  private parseBillsIssued() {
+    const billsIssued$ = this.domService.ids$.pipe(
+      this.unsubscribe.takeUntilDestroy,
+      map((el) => el.get(DashboardForm.BILLS_ISSUED) as HTMLInputElement)
+    );
+    billsIssued$.subscribe({
+      next: (htmlInput) => this.billsIssued.set(htmlInput.value),
+      error: (err) => console.error(err.message),
+    });
+  }
+  private parseStudents() {
+    const students$ = this.domService.ids$.pipe(
+      this.unsubscribe.takeUntilDestroy,
+      map((el) => el.get(DashboardForm.STUDENTS) as HTMLInputElement)
+    );
+    students$.subscribe({
+      next: (htmlInput) => this.students.set(htmlInput.value),
+      error: (err) => console.error(err.message),
+    });
+  }
+  private parseUsers() {
+    const users$ = this.domService.ids$.pipe(
+      this.unsubscribe.takeUntilDestroy,
+      map((el) => el.get(DashboardForm.USERS) as HTMLInputElement)
+    );
+    users$.subscribe({
+      next: (htmlInput) => this.users.set(htmlInput.value),
+      error: (err) => console.error(err.message),
+    });
+  }
+  private parseBankUsers() {
+    const bankUsers$ = this.domService.ids$.pipe(
+      this.unsubscribe.takeUntilDestroy,
+      map((el) => el.get(DashboardForm.BANK_USERS) as HTMLInputElement)
+    );
+    bankUsers$.subscribe({
+      next: (htmlInput) => this.bankUsers.set(htmlInput.value),
+      error: (err) => console.error(err.message),
+    });
+  }
+  private parseSchoolUsers() {
+    const schoolUsers$ = this.domService.ids$.pipe(
+      this.unsubscribe.takeUntilDestroy,
+      map((el) => el.get(DashboardForm.SCHOOL_UERS) as HTMLInputElement)
+    );
+    schoolUsers$.subscribe({
+      next: (htmlInput) => this.schoolUsers.set(htmlInput.value),
+      error: (err) => console.error(err.message),
+    });
+  }
+  private parseCollectedAmount() {
+    const collectedAmount$ = this.domService.ids$.pipe(
+      this.unsubscribe.takeUntilDestroy,
+      map((el) => el.get(DashboardForm.COLLECTED_AMOUNT) as HTMLInputElement)
+    );
+    collectedAmount$.subscribe({
+      next: (htmlInput) => this.collectedAmount.set(htmlInput.value),
+      error: (err) => console.error(err.message),
+    });
+  }
+  private attachEventHandlers() {
+    this.parseTotalSchools();
+    this.parseActiveSchools();
+    this.parseBillsIssued();
+    this.parseStudents();
+    this.parseUsers();
+    this.parseBankUsers();
+    this.parseSchoolUsers();
+    this.parseCollectedAmount();
+  }
+  ngAfterViewInit(): void {
+    this.domService.parseDocumentKeys(
+      this.keys,
+      Object.keys(DashboardForm).filter((key) => isNaN(Number(key))).length
+    );
+    this.attachEventHandlers();
   }
 }
